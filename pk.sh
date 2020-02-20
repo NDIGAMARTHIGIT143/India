01118531ab6bb1041d93e5be006d0e0c353b130b
NDIGAMARTHIGIT143 <nbabu045@gmail.com>
2020-02-20 08:34:23 +0530

    29

--- pk.sh
+++ pk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install apache2 -y
-sudo echo "<h1> Pink movie pk 27 </h1>" > /var/www/html/index.html
+sudo echo "<h1> Pink movie pk 29 </h1>" > /var/www/html/index.html
 sudo service apache2 restart
